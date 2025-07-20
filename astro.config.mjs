375c094af02b7ac823add6ca5d8ac36d93321dfc
Jeff <jeff@thepeakbeyond.com>
2025-07-19 22:48:50 -0700

    fix(seo): add site configuration to fix canonical URLs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://bright-gift.com',
   output: 'server',
   adapter: cloudflare({
     platformProxy: {
